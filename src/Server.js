a368866b31d9fed16994e716d056745b0d6cf81b
Matthew Crescimanno <matthew.crescimanno@leverege.com>
2018-04-29 19:21:57 -0400

    Added Express Server

---
+++ src/Server.js
@@ -0,0 +1,50 @@
+const cors = require( 'cors' )
+const helmet = require( 'helmet' )
+const express = require( 'express' )
+const compression = require( 'compression' )
+const log = require( './Log' )
+
+
+function start( options ) {
+  const app = express()
+  log.info( 'Server - Starting Up' )
+
+  const { corsOrigin = '*' } = options 
+
+  // Middleware
+  const corsOptions = {
+    origin : '*', // TODO : Change this to be a function that checks if corsOrigin variable has req.get('Origin')
+    allowHeaders : 'Accept, Content-Type, Origin, X-Requested-With',
+  }
+  app.use( cors( corsOptions ) )
+  app.use( helmet() )
+  app.use( compression() )
+  app.use( express.json() )
+  app.use( express.urlencoded( {extended : true} ) )
+
+  // Routes
+  app.options('*', cors() ) // Handle Preflight Requests (if need be)
+
+  app.get('/', (req, res, next) => {
+    res.send( 'OK' )
+  } )
+
+  // Custom Error Handler
+  app.use( (err, req, res, next) => {
+    // If we already streamed response, let express-default-error-handler handle it
+    if (res.headersSent) {
+      return next( err )
+    }
+    const status = err.status || 500
+    log.info( { err } )
+    res.status( status )
+    return res.json( { status } )
+  } )
+
+  // Listen
+  app.listen( 3000 )
+}
+
+module.exports = {
+  start
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 require( 'dotenv' ).config() // Gives us access to .env variables
 const log = require( './Log' ) // Logging Configuration
+const Server = require( './Server' )
 
+const logAndShutdown = ( err ) => {
+  console.log( 'Fatal Error', err )
+  log.fatal( err, 'Fatal Error' )
+  setTimeout( () => { process.exit(1) } , 100 )
+} 
 
-log.info( 'Server Started' )
-// console.log( [1,2,3].map( el => el ** 2 ), [{a:1},{a:2},{a:3}].find( el => el.a === 3 ) )
-// let y = { a : 1, b : 2, c : 3 }
-// let z = { ...y, a: 2 }
-// let x = `Im running version ${process.version}`
-// console.log( z )
-// console.log( process )
+// Register Exception Event Handlers
+process.on( 'uncaughtException', logAndShutdown ) // For Unhandled Synchronous Code
+process.on( 'unhandledRejection', logAndShutdown ) // For Unhandled Promises
+
+Server.start( {} )
